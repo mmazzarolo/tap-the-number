82027023efe2d93249232a0a72e6e6773e649af3
mmazzarolo <mazzarolomatteo@gmail.com>
2017-01-14 18:26:03 +0100

    Number colors

--- src/components/BoardTile/index.js
+++ src/components/BoardTile/index.js
@@ -2,8 +2,6 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react'
 import { View, Text } from 'react-native-animatable'
-import metrics from 'src/config/metrics'
-import type { Tile } from 'src/types'
 
 import styles from './index.style'
 
@@ -12,23 +10,27 @@ type Props = {
   bottom: number,
   backgroundColor: string,
   text: string | number,
-  style: ?any
+  style?: any
 }
 
 export default class BoardTile extends Component<void, Props, void> {
   render () {
     const { left, bottom, backgroundColor, text, style } = this.props
     const computedStyle = {
-      width: metrics.TILE_SIZE,
-      height: metrics.TILE_SIZE,
       left,
       bottom,
       backgroundColor
     }
+    const textColor = getContrastYIQ(backgroundColor)
     return (
       <View style={[computedStyle, styles.containerDefault, style]}>
-        <Text style={styles.text}>{text}</Text>
+        <Text style={[styles.text, { color: textColor }]}>{text}</Text>
       </View>
     )
   }
+}
+
+const getContrastYIQ = (hc) => {
+  const [r, g, b] = [1, 3, 5].map(p => parseInt(hc.substr(p, 2), 16))
+  return ((r * 299) + (g * 587) + (b * 114)) / 1000 >= 128 ? 'black' : 'white'
 }
--- src/components/BoardTile/index.style.js
+++ src/components/BoardTile/index.style.js
@@ -1,16 +1,22 @@
 import { StyleSheet } from 'react-native'
+import metrics from 'src/config/metrics'
 
 export default StyleSheet.create({
   containerDefault: {
     position: 'absolute',
     borderWidth: 1,
     borderColor: 'rgba(255, 255, 255, 0.2)',
-    shadowColor: 'rgba(0, 0, 0, 0.2)'
+    shadowColor: 'rgba(0, 0, 0, 0.2)',
     // shadowOpacity: 1,
     // shadowRadius: 0,
     // shadowOffset: {
     //   height: 4,
     //   width: 4
     // }
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: metrics.TILE_SIZE / 2,
+    width: metrics.TILE_SIZE,
+    height: metrics.TILE_SIZE
   }
 })
