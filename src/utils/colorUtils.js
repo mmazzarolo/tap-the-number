89c8142c0194e6799e4db8276a2fbcfad44fbb90
mmazzarolo <mazzarolomatteo@gmail.com>
2017-01-17 21:28:18 +0100

    iOS release build test

--- src/utils/colorUtils.js
+++ src/utils/colorUtils.js
@@ -1,5 +1,5 @@
 /* @flow */
-const getContrastYIQ = (hc) => {
+const getContrastYIQ = (hc: string) => {
   const [r, g, b] = [1, 3, 5].map(p => parseInt(hc.substr(p, 2), 16))
   return ((r * 299) + (g * 587) + (b * 114)) / 1000 >= 128 ? 'black' : 'white'
 }
